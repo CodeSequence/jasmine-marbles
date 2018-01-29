f6f06eeb24ed1c1a66ad39ce1318ab64e24ee6ee
bcabanes <contact@benjamincabanes.com>
2018-01-28 20:18:38 -0500

    feat(subscriptions): add ability to test subscriptions
    This adds the `toHaveSubscriptions` assertion to jasmine-marbles to be
    able to test the subscriptions of cold and hot observables.

--- index.ts
+++ index.ts
@@ -1,19 +1,40 @@
 import { Notification } from 'rxjs/Notification';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
+import { SubscriptionLog } from 'rxjs/testing/SubscriptionLog';
 import { TestMessage } from 'rxjs/testing/TestMessage';
 import { TestScheduler } from 'rxjs/testing/TestScheduler';
 
-import { getTestScheduler, initTestScheduler, resetTestScheduler } from './src/scheduler';
-import { TestColdObservable, TestHotObservable, TestObservable } from './src/test-observables';
+import {
+  getTestScheduler,
+  initTestScheduler,
+  resetTestScheduler,
+} from './src/scheduler';
+import {
+  TestColdObservable,
+  TestHotObservable,
+  TestObservable,
+} from './src/test-observables';
 
-export { getTestScheduler, initTestScheduler, resetTestScheduler } from './src/scheduler';
+export {
+  getTestScheduler,
+  initTestScheduler,
+  resetTestScheduler,
+} from './src/scheduler';
 
-export function hot(marbles: string, values?: any, error?: any): TestHotObservable {
+export function hot(
+  marbles: string,
+  values?: any,
+  error?: any,
+): TestHotObservable {
   return new TestHotObservable(marbles, values, error);
 }
 
-export function cold(marbles: string, values?: any, error?: any): TestColdObservable {
+export function cold(
+  marbles: string,
+  values?: any,
+  error?: any,
+): TestColdObservable {
   return new TestColdObservable(marbles, values, error);
 }
 
@@ -25,6 +46,7 @@ declare global {
   namespace jasmine {
     interface Matchers<T> {
       toBeObservable: any;
+      toHaveSubscriptions: any;
     }
   }
 }
@@ -66,8 +88,21 @@ function materializeInnerObservable(
 
 export function addMatchers() {
   jasmine.addMatchers({
+    toHaveSubscriptions: () => ({
+      compare: function(actual: TestObservable, marbles: string | string[]) {
+        const marblesArray: string[] =
+          typeof marbles === 'string' ? [marbles] : marbles;
+        const results: SubscriptionLog[] = marblesArray.map(marbles =>
+          TestScheduler.parseMarblesAsSubscriptions(marbles),
+        );
+
+        expect(results).toEqual(actual.getSubscriptions());
+
+        return { pass: true };
+      },
+    }),
     toBeObservable: () => ({
-      compare: function (actual: TestObservable, fixture: TestObservable) {
+      compare: function(actual: TestObservable, fixture: TestObservable) {
         const results: TestMessage[] = [];
         let subscription: Subscription;
         const scheduler = getTestScheduler();
