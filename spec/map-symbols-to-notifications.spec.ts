f07330a2c22dcca612a23cedab237bfb9596d092
Quentin Delettre <quentin.delettre@gmail.com>
2021-05-21 15:24:21 +0200

    chore: update jasmine to ^3.7.0 (#68)

--- spec/map-symbols-to-notifications.spec.ts
+++ spec/map-symbols-to-notifications.spec.ts
@@ -1,18 +1,34 @@
+import { NextNotification, ObservableNotification } from 'rxjs';
 import { TestScheduler } from 'rxjs/testing';
 import { mapSymbolsToNotifications } from '../src/map-symbols-to-notifications';
+
+function encodeSymbols(
+  marbles: string,
+  values: { [key: string]: any },
+): { [key: string]: ObservableNotification<any> } {
+  const expected = TestScheduler.parseMarbles(
+    marbles,
+    values,
+    undefined,
+    true,
+    true,
+  );
+
+  return mapSymbolsToNotifications(marbles, expected);
+}
 
 describe('Map symbols to frames', () => {
   it('should map single symbol', () => {
     const result = encodeSymbols('a', { a: 1 });
-    expect(result.a.value).toEqual(1);
+    expect((result.a as NextNotification<any>).value).toEqual(1);
   });
 
   it('should map multiple symbols', () => {
     const result = encodeSymbols('---a--bc-d|', { a: 1, b: 2, c: 3, d: 4 });
-    expect(result.a.value).toEqual(1);
-    expect(result.b.value).toEqual(2);
-    expect(result.c.value).toEqual(3);
-    expect(result.d.value).toEqual(4);
+    expect((result.a as NextNotification<any>).value).toEqual(1);
+    expect((result.b as NextNotification<any>).value).toEqual(2);
+    expect((result.c as NextNotification<any>).value).toEqual(3);
+    expect((result.d as NextNotification<any>).value).toEqual(4);
   });
 
   it('should support groups', () => {
@@ -22,32 +38,20 @@ describe('Map symbols to frames', () => {
       c: 3,
       d: 4,
     });
-    expect(result.a.value).toEqual(1);
-    expect(result.b.value).toEqual(2);
-    expect(result.c.value).toEqual(3);
+    expect((result.a as NextNotification<any>).value).toEqual(1);
+    expect((result.b as NextNotification<any>).value).toEqual(2);
+    expect((result.c as NextNotification<any>).value).toEqual(3);
   });
 
   it('should support subscription point', () => {
     const result = encodeSymbols('---a-^-b-|', { a: 1, b: 2 });
-    expect(result.a.value).toEqual(1);
-    expect(result.b.value).toEqual(2);
+    expect((result.a as NextNotification<any>).value).toEqual(1);
+    expect((result.b as NextNotification<any>).value).toEqual(2);
   });
 
   it('should support time progression', () => {
     const result = encodeSymbols('-- 100ms -a-^-b-|', { a: 1, b: 2 });
-    expect(result.a.value).toEqual(1);
-    expect(result.b.value).toEqual(2);
+    expect((result.a as NextNotification<any>).value).toEqual(1);
+    expect((result.b as NextNotification<any>).value).toEqual(2);
   });
-
-  function encodeSymbols(marbles: string, values: { [key: string]: any }) {
-    const expected = TestScheduler.parseMarbles(
-      marbles,
-      values,
-      undefined,
-      true,
-      true,
-    );
-
-    return mapSymbolsToNotifications(marbles, expected);
-  }
 });
